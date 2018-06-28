187e2544ea88bdff4911c6806e25261681d06f5b
Ryan Cast <ryancast@gmail.com>
2018-06-27 19:31:56 -0700

    late lights

--- apps/backtosleep.py
+++ apps/backtosleep.py
@@ -3,7 +3,7 @@ import appdaemon.plugins.hass.hassapi as hass
 class BackToSleep(hass.Hass):
 
     def initialize(self):
-        self.listen_state(self.you_are_feeling_sleepy, "binary_sensor.motion_sensor_158d00022399ea", new="off")
+        self.listen_state(self.you_are_feeling_sleepy, "binary_sensor.motion_sensor_158d0002119c02", new="off")
 
     def you_are_feeling_sleepy(self, entity, attribute, old, new, kwargs):
         sleeping = self.get_state("input_boolean.sleep_mode")
--- apps/latenightpisslight.py
+++ apps/latenightpisslight.py
@@ -3,7 +3,7 @@ import appdaemon.plugins.hass.hassapi as hass
 class LateNightPissLight(hass.Hass):
 
     def initialize(self):
-        self.listen_state(self.light_the_way, "binary_sensor.motion_sensor_158d00022399ea", new="on")
+        self.listen_state(self.light_the_way, "sensor.bedside_motion_sensors", new="on")
 
     def light_the_way(self, entity, attribute, old, new, kwargs):
         sleeping = self.get_state("input_boolean.sleep_mode")
