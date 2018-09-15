a54533ba636e73de4844bd8373a86ab578d7c83f
Ryan Cast <ryancast@gmail.com>
2018-09-15 12:04:25 -0700

    updated emptyplace trigger condition

--- apps/emptyplace.py
+++ apps/emptyplace.py
@@ -3,7 +3,7 @@ import appdaemon.plugins.hass.hassapi as hass
 class EmptyPlace(hass.Hass):
 
     def initialize(self):
-        self.listen_state(self.action, "!secret ryan_device_tracker", old="home")
+        self.listen_state(self.action, "binary_sensor.via_6_away", new="on")
 
     def action(self, entity, attribute, old, new, kwargs):
         self.turn_on("script.alarm_arm")
