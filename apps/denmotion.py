31efd22833b43dba8188d75383fc2ecc4ecf4eb2
Ryan Cast <ryancast@gmail.com>
2018-10-27 23:31:21 -0700

    bayesian sensor and alarm stuff

---
+++ apps/denmotion.py
@@ -0,0 +1,11 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+class DenMotion(hass.Hass):
+
+    def initialize(self):
+        self.listen_state(self.denmotionscript, "binary_sensor.motion_sensor_158d0002244f18", new = "on")
+
+    def denmotionscript(self, entity, attribute, old, new, kwargs):
+        alarm = self.get_state("input_boolean.alarm_on")
+        if alarm == "on":
+            self.turn_on("script.alarm_den")
