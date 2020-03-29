23fd701428e381589602cb31e97a8814edf6a7a8
r6t <ryancast@gmail.com>
2020-03-29 18:12:37 +0000

    wake sequence: add temp, make portable

--- apps/wakesequence.py
+++ apps/wakesequence.py
@@ -3,12 +3,16 @@ import appdaemon.plugins.hass.hassapi as hass
 class WakeSequence(hass.Hass):
 
     def initialize(self):
-        self.listen_state(self.wakeup, "sensor.bedside_motion_sensors", new = "on")
+        self.listen_state(self.wakeup, self.args["trigger"], new = "on")
 
     def wakeup(self, entity, attribute, old, new, kwargs):
-        soundcheck = self.get_state("media_player.sonos_den")
+        soundcheck = self.get_state(self.args["alarm_speaker"])
         print("soundcheck value is: ", soundcheck)
         if soundcheck == "playing":
-            self.turn_off("light.hallway")
+            for x in self.args["lights_ensure_off"]:
+                self.turn_off(x)
             for x in self.args["lights"]:
                 self.turn_on(x, brightness = 220, rgb_color = [255,207,120], transition = 15)
+            self.turn_on(self.args["thermostat"])
+            self.call_service("climate/set_hvac_mode", entity_id = self.args["thermostat"], hvac_mode = self.args["thermostat_mode"])
+            self.call_service("climate/set_temperature", entity_id = self.args["thermostat"], temperature = self.args["thermostat_temp"])
